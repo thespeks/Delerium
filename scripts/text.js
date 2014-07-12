5da238d9613975fae217b50ba320478332fd027d
TheSpeks <thespeks@users.noreply.github.com>
2014-07-12 02:29:20 -0400

    Create text.js

---
+++ scripts/text.js
@@ -0,0 +1,9 @@
+var _text = []
+
+
+var maintext {
+    append =        function (line) {_text.concat(text)},
+    extend =        function (lines) {
+        for (i=0; i<lines.length; i++) _text.concat(i)},
+
+}
