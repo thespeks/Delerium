dd7eaa4be4a065f3dd0f4c55e6a0856a54cf6b14
TheSpeks <thespeks@users.noreply.github.com>
2014-07-12 01:53:58 -0400

    Create colors.js

---
+++ scripts/constants/colors.js
@@ -0,0 +1,43 @@
+//Blondes
+var BLONDE =        { name: "blonde",       value: "#" }
+var BLONDE_LIGHT =  { name: "light blonde", value: "#" }
+var BLONDE_DARK =   { name: "dark blonde",  value: "#" }
+var BLONDES =       [BLONDE, BLONDE_LIGHT, BLONDE_DARK]
+
+
+//Blues
+var BLUE =          { name: "blue",         value: "#" }
+var BLUE_LIGHT =    { name: "light blue",   value: "#" }
+var BLUE_DARK =     { name: "dark blue",    value: "#" }
+var BLUE_SKY =      { name: "sky blue",     value: "#" }
+var BLUES =         [BLUE, BLUE_LIGHT, BLUE_DARK, BLUE_SKY]
+
+//Pinks
+var PINK =          { name: "pink",         value: "#" }
+var PINK_LIGHT =    { name: "light pink",   value: "#" }
+var PINK_HOT =      { name: "hot pink",     value: "#" }
+var PINKS =         [PINK, PINK_LIGHT, PINK_HOT]
+
+//Purples
+var PURPLE =        { name: ["purple", violet],                 value:"#" }
+var PURPLE_DARK =   { name: ["dark purple", "dark violet"],     value:"#" }
+
+//Grays
+var GRAY =          { name: "gray",         value: "#" }
+var GRAY_DARK =     { name: "dark gray",    value: "#" }
+var CHARCOAL =      { name: "charcoal",     value: "#" }
+GRAYS =             [GRAY, GRAY_DARK, CHARCOAL]
+
+//Greens
+GREEN =             { name: "green",        value: "#"}
+GREEN_OLIVE =       { name: "olive green",  value: "#"}
+
+//Yellows
+YELLOW =            { name: "yellow",       value: "#"}
+AMBER =             { name: "amber",       value: "#"}
+
+//Browns
+var BROWN =          { name: "brown",         value: "#" }
+var BROWN_DARK =     { name: "dark brown",    value: "#" }
+var BROWN_LIGHT =    { name: "light brown",     value: "#" }
+BROWNS =             [BROWN, BROWN_DARK, BROWN_LIGHT]
