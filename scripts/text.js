276c771d767ed69e551536125cf9a82ca08135f8
TheSpeks <thespeks@users.noreply.github.com>
2014-07-14 02:03:32 -0400

    Update text.js

--- scripts/text.js
+++ scripts/text.js
@@ -1,9 +1,32 @@
-var _text = []
-
-
-var maintext {
-    append =        function (line) {_text.concat(text)},
-    extend =        function (lines) {
-        for (i=0; i<lines.length; i++) _text.concat(i)},
-
+// add Array.isArray if not available
+if(!Array.isArray) {
+    Array.isArray = function(arg) {
+        return Object.prototype.toString.call(arg) === '[object Array]';
+    };
 }
+
+
+function TextLines (scrollback) = {
+    this.lines =        [];
+    this.scrollback =   scrollback;
+}
+
+TextLines.prototype.addlines = function (lines) {
+    if (Array.isArray(lines)) this.lines.concat(lines);
+    else this.lines.concat([lines]);
+    this.trim(this.get_sbdiff)
+}
+
+TextLines.prototype.get_sbdiff = function () {
+    return this.lines.length - this.scrollback;
+}
+
+TextLines.prototype.trim = function (n) {
+    // trim lines from beginning of array
+    if (n > 0) this.lines.splice(0, n); 
+}
+
+TextLines.prototype.clear = function () {
+    this.lines = []
+}
+
