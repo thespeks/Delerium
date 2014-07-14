49665331e517be5b1a44c69206d62c7d0fffb299
TheSpeks <thespeks@users.noreply.github.com>
2014-07-14 01:13:22 -0400

    Create main.js

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+
+var APP_NAME =          "Delerium"
+var VERSION_MAJOR =     0;
+var VERSION_MINOR =     1;
+var VERSION_NAME =      "";
+
+var SPC =               " " // shortcut for space
+
+var SHORT_VERSION_STRING = joinString([VERSION_MAJOR, ".", VERSION_MINOR], ""]);
+var VERSION_STRING = joinString([SHORT_VERSION_STIRNG, ": ", VERSION_NAME], "");
+
+
+
+var options = {     // commented is not yet implemented
+    // Debug and error related
+    //log_errors:             false,
+    //show_error_console:     false,
+    //debug_mode:             false,
+}
