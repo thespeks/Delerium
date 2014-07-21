ae05cfb15637858e66cde2917a2eedb769ea9e12
TheSpeks <thespeks@users.noreply.github.com>
2014-07-21 19:26:15 -0400

    Update char.js

--- scripts/char.js
+++ scripts/char.js
@@ -1,25 +1,66 @@
 
-var stat_proto {
-    name:                   null,
-    current:                0,
-    base:                   0,
-    _sv:               function (val , targ, clamp) {
-        if (!clamp) this.target = val;
-        else this.target = clamp(val, 0, 100);
-    }
-}
 // a value of 0 is default (where applicable)
-function char (name, age, personality) {
+
+/* ---- Location ------------------------------------------------------------ */
+function CharLoc () {
+    this.region =       0;
+    this.pos =          [0,0];
+    this.mapid:         0;
+    this.mapid2:        0;
+}
+
+// location prototypes
+CharLoc.prototype.getMap = function () {
+    if (this.mapid2 > 0) return this.getRegion().maps[mapid]
+    return this.getRegion().maps[mapid]
+}
+
+CharLoc.prototype.getRegion = function () {
+    return regions[this.region]
+}
+
+
+/* ---- Inventory ----------------------------------------------------------- */
+function CharInv () {
+    this.slots =              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+}
+
+// inventory prototypes
+CharInv.prototype.is_valid = function(i) {
+    if (0 <= slot_index <= 9) return true;
+    return false;
+}
+
+CharInv.prototype.set_slot = function (slot_index, value) {
+    if (this.is_valid(slot_index)) slots[slot_index] = value;
+}
+
+CharInv.prototype.get_free_slot = function () {
+    for (var slot in this.slots) {
+        if this.slots[slot] == 0: return slot;
+    return null;
+}
+
+CharInv.prototype.swap_slot = function (a, b) {
+    if (this.is_valid(a) && this.is_valid(b)) {
+        var temp = this.slots[b];
+        this.slots[b] = this.slots[a];
+        this.slots[a] = temp;
+}
+
+
+/* ---- Char ---------------------------------------------------------------- */
+function Char (name, age, personality, species, types) {
     // Misc data
     this.name =             name;
     this.age =              age;
     this.personality =      personality;
-    this.location = {
-        region:             0,
-        pos:                [0,0],
-        mapid:              0,
-        mapid2:             0
-    }
+    this.species =          species;
+    this.types =            types;
+
+    this.loc =              CharLoc();
+    this.inventory =        CharInv();
+
     this.stats = {
         health:             100,
         energy:             100,
@@ -29,24 +70,30 @@ function char (name, age, personality) {
         rflx:               50,             // reflex
         agil:               30              // agility
     }
+
     this.statuses = {
+        cursed:             0,
         drunk:              0,
         poison:             0,
     }
+
     this.clothing = {
-            tops:           0,
-            bottoms:        0,
-            undies_top:     0,
-            undies_bottom:  0,
-            socks:          0,
-            shoes:          0,
-            glasses:        0
-    }
-    this.inventory = {
-        slots:              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        tops:           0,
+        bottoms:        0,
+        undies_top:     0,
+        undies_bottom:  0,
+        socks:          0,
+        shoes:          0,
+        glasses:        0
     }
 
     // physical appearance
+    this.height =       66;     // in inches
+    this.tone =         20;     // muscle tone
+    this.skin = {
+        color:          0;
+    }
+
     this.ears =
         type:           0,
         left = {
@@ -73,18 +120,11 @@ function char (name, age, personality) {
     this.tongue = {
         piercing:       0,
         length:         1.5,
-        color:          "Pink";
+        color:          "Pink"
     }
     this.belly = {
         piercing:       0,      //naval
         tone:           20
-    }
-
-
-
-
-    this.height =       66;     // in inches
-    this.tone =         20;     // muscle tone
     }
 
     this.jobskills {
@@ -93,38 +133,16 @@ function char (name, age, personality) {
         bartending:     0
     }
 
-    //toJSON:             function () {
-    //}
 }
 
 
-
-// location prototypes
-char.location.prototype.getmap = function() {
-    if (this.mapid2 > 0) return this.region.mapid.mapid2;
-    return this.region.mapid.mapid2;
+var stat_proto {
+    name:                   null,
+    current:                0,
+    base:                   0,
+    _sv:               function (val , targ, clamp) {
+        if (!clamp) this.target = val;
+        else this.target = clamp(val, 0, 100);
+    }
 }
 
-
-// inventory prototypes
-char.inventory.prototype.is_valid = function(i) {
-    if (0 <= slot_index <= 9) return true;
-    return false;
-}
-
-char.inventory.prototype.set_slot = function (slot_index, value) {
-    if (this.is_valid(slot_index)) slots[slot_index] = value;
-}
-
-char.inventory.prototype.get_free_slot = function () {
-    for (var slot in this.slots) {
-        if this.slots[slot] == 0: return slot;
-    return null;
-}
-
-char.inventory.prototype.swap_slot = function (a, b) {
-    if (this.is_valid(a) && this.is_valid(b)) {
-        var temp = this.slots[b];
-        this.slots[b] = this.slots[a];
-        this.slots[a] = temp;
-}
