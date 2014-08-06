fd0458991821674709d34314811b3e2f3b3001ef
TheSpeks <thespeks@users.noreply.github.com>
2014-08-05 20:55:01 -0400

    Update location.js

--- scripts/location.js
+++ scripts/location.js
@@ -19,41 +19,75 @@ var re = new Region ("");
 var regions = [ra,rb,rc,rd,re];
 
 
-/* -------------------------------------------------------------------------- */
-map = function (id, cells, type) {
-    this.id =        id;
-    this.cells =     cells;
-    this.type =      type;
+/* -- Map ------------------------------------------------------------------- */
+mapType = function (name) {
+    this.name = name;
 }
 
-// map prototypes
-map.prototype.get_cell:   function (coords) {
+maptypes.TOWN =         mapType("town");
+maptypes.FOREST =       mapType("forest");
+maptypes.PLAINS =       mapType("plains");
+maptypes.MOUNTAIN =     mapType("mountain");
+maptypes.PERMAFROST =   mapType("permafrost");
+maptypes.CAVE =         mapType("cave");
+
+
+Map = function (region, name, id, types) {
+    this.region =       region;
+    this.name =         name;
+    this.id =           id;
+    this.cells =        [];
+    this.types =        types;
+    this.submaps =      [];
+}
+
+Map.prototype.get_cell:   function (coords) {
     for (i=0; i<this.cells.length; i++) {
         if (this.cells[i].pos == coords) return i;
     return null;
     }
 }
 
-map.prototype.has_cell:   function (coords) {
+Map.prototype.has_cell:   function (coords) {
     if (this.get_cell(coords) ! null) return true;
     return false;
 }
 
 
-/* -------------------------------------------------------------------------- */
-var cell {
-    type:                   0,
-    flags:                  [],
-    pos:                    [0,0],
-    is_exit:                [0,0,0,0],      // NESW
-    _visited:               [],
-    _flags:                 []              // char flags?
+/* -- Cell ------------------------------------------------------------------ */
+cellType = function (num, name, tagids) {
+    this.num =      num;
+    this.name =     name;
+    this.tagids =   tagids;
 }
 
-// cell prototypes
-cell.prototype.toJSON = function () {
-    yield this._visited;
-    yield this._flags;
+var HISTORY =       1;
+var NATURE =        2;
+var WALK =          3;
+var DANCING =       4;
+var XXX =           5;
+var RELAX =         6;
+var WORKOUT =       7;
+var DINING =        8;
+var DRINK =         9;
+var ENTERTAINMENT = 10;
+var SHOP =          11;
+var POTIONS =       12;
+var SUNDRIES =      13;
+var CLOTHING =      14;
+var LINGERIE =      15;
+var JEWELRY =       16;
+
+var HOTEL =         30;
+var REST =          31;
+var LEARNING =      32;
+var TRAVEL =        33;
+
+cell = function (pos, type, is_exit) {
+    this.type =         type;
+    this.flags =        [];
+    this.pos =          pos;
+    this.is_exit =      [0,0,0,0];      // NESW
 }
 
 cell.prototype.get_adjacent_coords = function (x, y) {
